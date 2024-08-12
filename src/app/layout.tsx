c6c9af8b9b9f6ae2ff25f3bcd9495e2db782a805
Sujeet Yadav <androexp3rt@gmail.com>
2024-08-12 19:27:12 +0530

    update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,7 +26,6 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
-        <script src="https://telegram.org/js/telegram-web-app.js"></script>
       </head>
       <body className={inter.className}>
         <Providers>{children}</Providers>
