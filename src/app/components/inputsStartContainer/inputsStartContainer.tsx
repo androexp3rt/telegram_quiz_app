9d9f560a5e2ec7aa3457e252be8f426670afd9dc
Sujeet Yadav <androexp3rt@gmail.com>
2024-08-22 21:51:15 +0530

    major update

--- src/app/components/inputsStartContainer/inputsStartContainer.tsx
+++ src/app/components/inputsStartContainer/inputsStartContainer.tsx
@@ -3,49 +3,25 @@ import { useState, useEffect } from "react";
 import { useRouter } from "next/navigation"
 import SelectInput from "./selectInput";
 import IdInput from "./idInput";
-import checkSubs from "../../../../lib/checkSubs"
 import SubscribeBtnBox from "../subscribeBtn";
 
-declare global {
-  interface Window {
-    Telegram: {
-      WebApp: {
-        initDataUnsafe: {
-          user: {
-            first_name: string;
-            last_name: string;
-            id:number;
-          };
-        };
-      };
-    };
-  }
+type Params = {
+  isSubscriber: boolean
 }
 
-export default function InputsStartContainer() {
+
+export default function InputsStartContainer({isSubscriber}:Params) {
   const [quizId, setQuizId] = useState<number>(0)
   const [quizIdMin, setQuizIdMin] = useState<number>(11407)
   const [quizIdMax, setQuizIdMax] = useState<number>(12598)
-  const [player, setPlayer] = useState<TelegramUser | null>(null)
-  const [isSubscriber, setIsSubscriber] = useState<boolean>(false)
   const router = useRouter();
-  useEffect(() => {
-    if (typeof window !== 'undefined' && window.Telegram && window.Telegram.WebApp) {
-      const { initDataUnsafe } = window.Telegram.WebApp;
-      const { user } = initDataUnsafe;
-      setPlayer(user as TelegramUser);
-      checkSubs({userId:user.id}).then(sub => {
-        setIsSubscriber(sub);
-      });
-    }
-  }, []);
+  
   function startQuiz() {
     router.push(`/${quizId}`);
   }
   return (
     <>
     <div className="w-full h-80 my-5 flex flex-col align-center justify-around">
-      <p className="w-full flex justify-center font-bold text-lg">{`Welcome ${player?.username}!`}</p>
       <SelectInput handleQuizId={setQuizId} />
       <p className="w-full flex justify-center text-lg text-center font-bold">
         Or
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,15 +3,46 @@ import Heading from "./components/heading"
 import InputsStartContainer from "./components/inputsStartContainer/inputsStartContainer"
 import ErrorBoundary from "./components/ErrorBoundary"
 import Navbar from "./components/Navbar"
+import checkSubs from "../../lib/checkSubs"
+import { useState, useEffect } from "react";
+
+declare global {
+  interface Window {
+    Telegram: {
+      WebApp: {
+        initDataUnsafe: {
+          user: {
+            first_name: string;
+            last_name: string;
+            id:number;
+          };
+        };
+      };
+    };
+  }
+}
 
 export default function Home() {
+  const [player, setPlayer] = useState<TelegramUser | null>(null)
+  const [isSubscriber, setIsSubscriber] = useState<boolean>(false)
+  useEffect(() => {
+    if (typeof window !== 'undefined' && window.Telegram && window.Telegram.WebApp) {
+      const { initDataUnsafe } = window.Telegram.WebApp;
+      const { user } = initDataUnsafe;
+      setPlayer(user as TelegramUser);
+      checkSubs({userId:user.id}).then(sub => {
+        setIsSubscriber(sub);
+      });
+    }
+  }, []);
   return (
     <ErrorBoundary>
       <main className="min-h-screen">
         <Navbar back={false} />
         <section className="p-5">
           <Heading />
-          <InputsStartContainer />
+          <p className="w-full flex justify-center font-bold text-lg">{`Welcome ${player?.username}!`}</p>
+          <InputsStartContainer isSubscriber={isSubscriber}/>
         </section>
       </main>
     </ErrorBoundary>
